28592d1bc7e216d3101a9949ed01fbdd838c9df5
PulakC01 <PulakC01@users.noreply.github.com>
2025-08-28 11:16:55 +0530

    Updated References

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    key            = "DevSecOps-Pipeline-for-Tetris-on-AWS/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
